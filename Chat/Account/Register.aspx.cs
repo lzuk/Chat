43e14e43f1d363a70d1238353f9a97d50b3b6818
Łukasz Żuk <lzuk@future-processing.com>
2013-11-14 14:26:19 +0100

    Bootstrap

---
+++ Chat/Account/Register.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Security;
+using System.Web.UI;
+
+namespace Chat
+{
+    public partial class Register : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (Membership.GetUser(nickBox.Text) != null)
+            {
+                
+            }
+            else
+            {
+                string nick = nickBox.Text;
+                string password = passwordBox.Text;
+                
+                Membership.CreateUser(nick, password, mailBox.Text);
+                if (Membership.ValidateUser(nick, password))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(nick, true);
+                }
+                else
+                {
+                    //msg.Text = "Invalid error";
+                }
+            }          
+        }
+    }
+}
